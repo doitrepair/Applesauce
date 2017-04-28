2305bab8d425d70ec6b259b42c33a31b530125f1
Justin Essert <justinkessert@gmail.com>
2017-04-28 15:32:31 -0500

    fixed db name errors

--- config.js
+++
@@ -1,7 +0,0 @@
-//Defining configuration variables
-module.exports = {
-    'port':			process.env.PORT || 8080,
-    'db':			"applesauce_doit_wisc_edu_",
-    'password':     "password",
-    'hostname':     "linux3.dwh.doit.wisc.edu"
-};
