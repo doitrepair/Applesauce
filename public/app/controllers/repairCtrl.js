f20c8626555978cec2b28576a75cb50d3515756c
Justin Essert <justinkessert@gmail.com>
2017-11-22 16:08:29 -0600

    added support for alt-contact field

--- public/app/controllers/repairCtrl.js
+++ public/app/controllers/repairCtrl.js
@@ -17,8 +17,10 @@ angular.module('repairCtrl', ['dbService', 'submitRepair'])
 		vm = this;
 
 		var description_suffix = " - NEXT AGENT - please update any 'Needs Update' fields as well as the Approved Price Ceiling and Repair Coverage when the customer comes in to drop off their computer";
+		$scope.questions = 'N/A';
 
 		$scope.submit_repair = function() {
+			var alt_contact = 'Email='+$scope.email+' Phone='+$scope.tel;
 			if ($scope.netId == undefined || $scope.contactPref == undefined || $scope.os == undefined)
 				return;
 			var full_description = $scope.description + description_suffix;
@@ -26,6 +28,7 @@ angular.module('repairCtrl', ['dbService', 'submitRepair'])
 				`<br>description_key:`+ full_description +`--eol<br>
 				short_description_key: Online Repair - Needs Update--eol<br>
 				net_id_key:  `+ $scope.netId +`--eol<br>
+				alt_cont_key: `+ alt_contact +`--eol<br>
 				os_key:  `+ $scope.os +`--eol<br>
 				make_key:  `+ $scope.make +`--eol<br>
 				model_key: Needs Update--eol<br>
@@ -42,7 +45,7 @@ angular.module('repairCtrl', ['dbService', 'submitRepair'])
 			$location.path('/success');
 		};
 
-		var description = "";
+		var questions = "";
 
 		// get the repair information that the questions controller collected
 		vm.repairData = qaFactory.loadRepairData();
@@ -60,14 +63,15 @@ angular.module('repairCtrl', ['dbService', 'submitRepair'])
 				// Scope variable for the custom text for this repair
 				$scope.repair_text = vm.repair.definition;
 
-				// Set repair description from data received from q&a controller
+				// Set repair questions from data received from q&a controller
 				var index = 0;
 				vm.repairData.question_data.forEach(function(element) {
 					var answer_text = vm.repairData.answer_data[index].answer_text;
 					index++;
-					description = description + "Question - " + element.question_text + " \n" + "Answer - " + answer_text + " \n";
+					questions = questions + "Question - " + element.question_text + " <br />" + "Answer - " + answer_text + " <br />";
 				});
-				$scope.description = description;
+				console.log(questions);
+				$scope.questions = questions;
 			});
 
 		}
