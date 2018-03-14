8a775147af650163afdd8b422f36e4734b167d9b
Justin Essert <justinkessert@gmail.com>
2018-03-13 20:38:31 -0500

    fixed flashing issue

--- public/app/controllers/mainCtrl.js
+++ public/app/controllers/mainCtrl.js
@@ -1,6 +1,6 @@
 angular.module('mainCtrl', ['configService'])
 	.controller('mainController', function($scope, env) {
-		$scope.prod_env = env.prod;
+		$scope.dev_env = !env.prod;
 		$scope.post = env.post;
 		$scope.post_message = "Cite Inactive";
 		if($scope.post) $scope.post_message = "Cite Active";
@@ -10,6 +10,5 @@ angular.module('mainCtrl', ['configService'])
 			$scope.post = env.post;
 			if($scope.post) $scope.post_message = "Cite Active";
 			else $scope.post_message = "Cite Inactive";
-			console.log(env.post);
 		}
 	});
