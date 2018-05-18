9a49f8e18e7bed8e93bc173f3b7f6f70eea6d354
Justin Essert <justinkessert@gmail.com>
2018-05-18 09:49:17 -0500

    homeApp impl and serving correctly, other apps not serving correctly from express

--- server.js
+++ server.js
@@ -68,19 +68,21 @@ app.use('/api/email', emailRouter);
 // Catch all route: if any other path, send index.html
 
 // Tools Routes
+app.use('/tools*', express.static(__dirname + '/dist/toolsApp/'));
 app.get('/tools*', function(req, res) {
-  app.use(express.static(__dirname + '/dist/toolsApp/'));
+  //app.use(express.static(__dirname + '/dist/toolsApp/'));
 	res.sendFile(path.join(__dirname + '/dist/toolsApp/index.html'));
 });
 
 // Net-Id Authenticated Routes
+app.use('/net*', express.static(__dirname + '/dist/netIdApp/'));
 app.get('/net*', function(req, res) {
-  app.use(express.static(__dirname + '/dist/netIdApp/'));
+  //app.use(express.static(__dirname + '/dist/netIdApp/'));
 	res.sendFile(path.join(__dirname + '/dist/netIdApp/index.html'));
 });
 
 // Default Route
-app.use(express.static(__dirname + '/dist/homeApp'));
+app.use('/', express.static(__dirname + '/dist/homeApp'));
 app.get('*', function(req, res) {
 	res.sendFile(path.join(__dirname + '/dist/homeApp/index.html'));
 });
